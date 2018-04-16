47cec31bd6104306aa691b67ee57c0e1a1b6a915
王晨 <2090861117@qq.com>
2018-04-16 23:22:03 +0800

    做好了文章模型相关的功能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,41 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
-    
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      redirect_to 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 
